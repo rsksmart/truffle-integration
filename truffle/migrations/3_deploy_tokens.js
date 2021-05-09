8bf4a5f7a732a891b9e75d08899cf6130d4a4b01
Rémi COHEN-SCALI <remi@cohenscali.net>
2021-05-09 20:20:03 +0200

    Updated ERC20 deployer script
    Adapt deployer script to adapt to erc20 constructor params

--- truffle/migrations/3_deploy_tokens.js
+++ truffle/migrations/3_deploy_tokens.js
@@ -1,5 +1,5 @@
 const ERC20 = artifacts.require('./tokens/ERC20.sol');
 
 module.exports = (deployer) => {
-  deployer.deploy(ERC20, 10000, 'Simon Bucks', 1, 'SBX');
+  deployer.deploy(ERC20, 'Simon Bucks', 'SBX');
 };
